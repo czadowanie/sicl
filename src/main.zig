dedbccf816d4999624f61370b2fc7d888b20c1bc
czadowanie <nikolakisala@gmail.com>
2023-06-15 00:57:10 +0200

    no more magic numbers :)

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,10 @@
 const std = @import("std");
 const mem = std.mem;
 
+const MAX_CONFIG_SIZE = 1024 * 1024;
+const MAX_CSV_SIZE = 1024 * 1024;
+const MAX_MENU_OUTPUT_SIZE = 1024;
+
 const ParseOutput = struct {
     menu_input: []const u8,
     keys: [][]const u8,
@@ -66,7 +70,7 @@ fn runMenu(
     child.stdin.?.close();
     child.stdin = null;
 
-    const raw_output = try child.stdout.?.readToEndAlloc(allocator, 1024);
+    const raw_output = try child.stdout.?.readToEndAlloc(allocator, MAX_MENU_OUTPUT_SIZE);
     const output = std.mem.trim(u8, raw_output, " \n\t");
 
     _ = try child.kill();
@@ -94,7 +98,7 @@ pub fn run(
         },
         else => return err,
     };
-    const content = try file.readToEndAlloc(arena.allocator(), 1024 * 1024);
+    const content = try file.readToEndAlloc(arena.allocator(), MAX_CSV_SIZE);
     const parsed = try parseCsv(content, arena.allocator());
 
     const output = try runMenu(
@@ -160,7 +164,7 @@ const SiclConfig = struct {
     fn updateWithConfig(self: *@This(), allocator: mem.Allocator, path: []const u8) !void {
         var file = try std.fs.openFileAbsolute(path, .{});
         defer file.close();
-        const content = try file.readToEndAlloc(allocator, 1024 * 1024);
+        const content = try file.readToEndAlloc(allocator, MAX_CONFIG_SIZE);
         const options = try std.json.parseFromSlice(SiclConfig, allocator, content, std.json.ParseOptions{});
 
         self.menu_cmd = options.menu_cmd orelse self.menu_cmd;
@@ -191,7 +195,7 @@ fn show_help() !void {
 }
 
 fn runMenuAndSelecion(allocator: mem.Allocator, config: SiclConfig) !void {
-    var output_allocation = try allocator.alloc(u8, 1024);
+    var output_allocation = try allocator.alloc(u8, MAX_MENU_OUTPUT_SIZE);
     const menu_cmd = try cmdToArgv(allocator, config.menu_cmd.?);
     if (try run(allocator, config.csv_path.?, menu_cmd.items, output_allocation)) |command| {
         var run_args = try std.ArrayList([]const u8).initCapacity(allocator, 32);
@@ -207,7 +211,6 @@ fn runMenuAndSelecion(allocator: mem.Allocator, config: SiclConfig) !void {
 }
 
 fn addEntry(args: []const []const u8, config: SiclConfig) !void {
-    // add an entry
     if (args.len != 4) {
         try show_help();
         return;
@@ -238,7 +241,7 @@ fn removeEntry(allocator: mem.Allocator, args: []const []const u8, config: SiclC
         config.csv_path.?,
         .{ .mode = std.fs.File.OpenMode.read_write },
     );
-    const content = try csv.readToEndAlloc(allocator, 1024 * 1024);
+    const content = try csv.readToEndAlloc(allocator, MAX_CSV_SIZE);
     const parsed = try parseCsv(content, allocator);
 
     try csv.seekTo(0);
